8457991ab709449f3c2d4e04eb2c16771e439fa4
newt196 <152910318+newt196@users.noreply.github.com>
2024-12-06 07:46:31 -0600

    Create ANS2.py

---
+++ Leetcode & Algorithms/4. Median of Two Sorted Arrays/ANS2.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        sort = sorted(nums1 + nums2)
+        
+
+        
+        n = len(sort)
+        median = n // 2
+
+        if n % 2 == 1:
+            return float(sort[median])
+        else:
+            return (sort[median - 1] + sort[median]) / 2
